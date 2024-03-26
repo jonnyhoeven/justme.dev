196bc68b434638ae8461062a74d45bbe671322be
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2024-03-26 19:05:43 +0100

    remove unused packages

--- .vitepress/transformPageData.js
+++ .vitepress/transformPageData.js
@@ -1,9 +1,7 @@
 import transformPage from "../lib/transformPage";
-import fetchReadme from "../lib/fetchReadme.js";
 
 export default async (pageData, {siteConfig}) => {
     pageData = transformPage(pageData);
-    pageData.frontmatter.readme = pageData.frontmatter.fetchReadme ?  await fetchReadme(pageData.frontmatter.readmeUrl) : null;
     return pageData;
 }
 
