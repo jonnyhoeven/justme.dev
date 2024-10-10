84e413d74c47263d0141a48ae6ed208077eb8c65
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2024-10-10 20:38:47 +0200

    add todo

--- openai/process.py
+++ openai/process.py
@@ -16,6 +16,13 @@ class MLResponseHandler:
         self.client = OpenAI(api_key=self.api_key, base_url=self.api_url)
         self.max_width = 120
 
+    # @todo: replace internal links in markdown with external git url
+    # We need to change the internal links to external links by prefixing the right git url for every non https link
+    # in the readme file. We can use the get_readme method to fetch the readme file from the git repository and then
+    # replace the internal links with the right git url.
+    # since links in markdown files are in the format [link text](link url), we can use regex to match the links and
+    # then replace the link url with the right git url.
+
     def get_readme(self, user, project, branch, readme_file):
         if not all([user, project, readme_file]):
             print(f'Warning: Missing user, project, or readme_file')
