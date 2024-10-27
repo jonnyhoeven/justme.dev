0ec87e0ef109c09ef91f9c270d085808fc3b1d00
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2024-10-27 02:24:19 +0200

    don't load theme fonts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme-without-fonts'
 import './style.css'
 
 export default {
