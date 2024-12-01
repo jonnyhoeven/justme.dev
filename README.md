4c8f4dd296c1dd26b0f70a2a4e84ff5846a152c8
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2024-12-01 11:53:46 +0100

    remove openai

--- README.md
+++ README.md
@@ -2,33 +2,12 @@ This VitePress site is hosted from GitHub.io pages at [justme.dev](https://justm
 
 ## Requirements
 
-### OpenAI API key
-
-Only needed if generating content from OpenAI API's.
-
-```bash
-cp .env.example .env
-```
-
-Copy the [.env.example](https://github.com/jonnyhoeven/justme.dev/blob/main/.env.example) file to `.env` and fill in the
-required fields.
-
 ### Node version manager
 
 ```bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 nvm install
 nvm use
-```
-
-### Python & pip
-
-*Only needed if generating content from OpenAI or pulling contents.*
-
-```bash
-sudo apt install python3
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
 ```
 
 #### Install dependencies
@@ -41,7 +20,7 @@ npm install
 
 ### Generate content
 
-Downloads readme files from GitHub repositories and when configured connects to the OpenAI API to generate content.
+Downloads readme files from GitHub repositories.
 
 ```bash
 npm run docs:generate
@@ -72,11 +51,9 @@ trigger [deployment](https://github.com/jonnyhoeven/justme.dev/actions/workflows
 
 ## Notes
 
-- Create request files in the `requests` folder,
-  [open-ai-usage.yaml.example](https://github.com/jonnyhoeven/justme.dev/blob/main/openai/request/open-ai-usage.yaml.example).
+- Create request files in the `requests` folder.
 - Run the `npm run docs:generate` command to generate the markdown files locally.
 - When pushing to `main` GitHub action does `generate`, `build` and `deploy`.
-- The OpenAI response will be saved in the response folder.
 - The generated markdown will be saved in a sub folder defined by `request type` using the same name as the request file
   and the suffix `.md`.
 - Delete corresponding `response.bin` in `./openai/response` to force the script to retrieve data from `openai`
--- generate/process.py
+++ generate/process.py
@@ -1,24 +1,13 @@
 import os
-import pickle
 import requests
 import textwrap
 import yaml
-from dotenv import load_dotenv
-from openai import OpenAI
 
 
-class MLResponseHandler:
+class GenerateFiles:
     def __init__(self):
-        load_dotenv()
-        self.req_path = os.environ['REQUEST_PATH']
-        self.api_key = os.environ['OPENAI_API_KEY']
-        self.api_url = os.environ['API_URL']
         self.max_width = 120
-
-        if os.environ['API_URL']:
-            print(f'Enabling OpenAI client...')
-            self.client = OpenAI(api_key=self.api_key, base_url=self.api_url)
-
+        self.req_path = "generate/requests"
     def get_readme(self, user, project, branch, readme_file):
         if not all([user, project, readme_file]):
             print(f'Warning: Missing user, project, or readme_file')
@@ -29,29 +18,6 @@ class MLResponseHandler:
             print(f'Error: No 200 response {url}')
             return ''
         return response.text
-
-    def load_ml_response(self, response_path):
-        with open(response_path, 'rb') as file:
-            return pickle.load(file)
-
-    def retrieve_ml_response(self, model, messages):
-        return self.client.chat.completions.create(model=model, messages=messages)
-
-    @staticmethod
-    def parse_ml_response(response):
-        data = dict()
-        message = response.choices[0].message.content
-        if 'Blog blog:' in message:
-            data['intro'] = message.split('Blog Post:')[0].split('Intro:')[1].strip()
-            data['post'] = message.split('Blog Post:')[1].strip()
-        else:
-            data['post'] = message
-        data['total_tokens'] = response.usage.total_tokens
-        return data
-
-    def save_ml_response(self, path, response):
-        with open(path, 'wb') as file:
-            pickle.dump(response, file)
 
     def process_requests(self):
         for req_file in [f for f in os.listdir(self.req_path) if
@@ -71,30 +37,13 @@ class MLResponseHandler:
     def process_request(self, request, req_file):
         my_wrap = textwrap.TextWrapper(width=self.max_width)
         basename = req_file.split('.')[0]
-        res_path = f'openai/response/{basename}.bin'
         dst_path = f'{request["type"]}s/{basename}.md'
         readme = ''
         post = ''
-        res = {}
         print(f'Processing {basename}')
         if request.get('fetchReadme', False):
             readme = self.get_readme(request['user'], request['project'], request['branch'], request['readmeFile'])
-        if request.get('fetchML', False):
 
-            if os.path.isfile(res_path):
-                res = self.load_ml_response(res_path)
-            else:
-                print(f'Fetching response from api: {res_path}')
-                if not request['model'] or not request['messages']:
-                    print(f'Error: Missing model or messages in {req_file}')
-                    return
-                res = self.retrieve_ml_response(request['model'], request['messages'])
-                self.save_ml_response(res_path, res)
-            if res:
-                ml_res = self.parse_ml_response(res)
-                request['total_tokens'] = ml_res['total_tokens']
-                request['intro'] = ml_res['intro']
-                post = ml_res['post']
         self.save_post(request, dst_path, post, readme, my_wrap)
 
     @staticmethod
@@ -116,5 +65,5 @@ class MLResponseHandler:
 
 
 if __name__ == "__main__":
-    handler = MLResponseHandler()
+    handler = GenerateFiles()
     handler.process_requests()
