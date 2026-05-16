49bd5675072e9af8108f9dacd07ab69a89562690
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2026-05-16 08:42:53 +0200

    convert is being deprecated, change to magick, preserve source image file transparancy

--- gen_image.sh
+++ gen_image.sh
@@ -6,9 +6,10 @@ SIZE_HEIGHT=378
 for img in ./input_images/*.png; do
     [ -e "$img" ] || continue
     filename=$(basename "$img" .png)
-    convert "$img" \
+    magick "$img" \
         -resize "${SIZE_WIDTH}x${SIZE_HEIGHT}^" \
         -gravity center \
+        -background transparent \
         -extent "${SIZE_WIDTH}x${SIZE_HEIGHT}" \
         "./public/images/${filename}.webp"
     echo "Done: ./public/images/${filename}.webp"
